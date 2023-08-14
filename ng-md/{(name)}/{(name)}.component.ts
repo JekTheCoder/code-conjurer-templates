6ffaaafd296654f72de2102d5f9b237fbf3090a5
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-08-14 15:35:18 -0500

    fix: ng-md import mat-button module

--- ng-md/{(name)}/{(name)}.component.ts
+++ ng-md/{(name)}/{(name)}.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppDialogComponent } from '@shared/modules/app-dialog/dialog-component';
 import { AppDialogRef } from '@shared/modules/app-dialog/dialog-ref';
